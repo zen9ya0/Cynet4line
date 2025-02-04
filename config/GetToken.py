a7e402b27627094f787317054b12d5097a59c691
zen9ya0 <solar324yao@gmail.com>
2025-02-04 17:13:10 +0800

    Update GetToken.py

--- config/GetToken.py
+++ config/GetToken.py
@@ -1,6 +1,14 @@
 import http.client
 import json
-from config.config import user_name, password 
+import os  # 用於讀取環境變數
+
+# 從環境變數中讀取 Cynet_user_name 和 Cynet_password
+user_name = os.getenv('Cynet_user_name')
+password = os.getenv('Cynet_password')
+
+if user_name is None or password is None:
+    print("請確保環境變數 Cynet_user_name 和 Cynet_password 已正確設置。")
+    exit(1)
 
 conn = http.client.HTTPSConnection("mssp.api.cynet.com")
 
